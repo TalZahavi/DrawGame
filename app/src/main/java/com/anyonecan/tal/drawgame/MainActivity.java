ff466885d0357f4e8849ee4da9b84453da05a2d9
Tal Zahavi <tal.zahavi9@gmail.com>
2015-06-20 22:01:04 +0300

    Add animation to main screen

--- app/src/main/java/com/anyonecan/tal/drawgame/MainActivity.java
+++ app/src/main/java/com/anyonecan/tal/drawgame/MainActivity.java
@@ -1,12 +1,15 @@
 package com.anyonecan.tal.drawgame;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ImmersiveActivity implements View.OnClickListener {
+public class MainActivity extends ImmersiveActivity {
 
     Button card1;
     Button card2;
@@ -19,6 +22,8 @@ public class MainActivity extends ImmersiveActivity implements View.OnClickListe
     Button card9;
     Button card10;
 
+    Rect rect;
+
     boolean continueMusic;
 
     @Override
@@ -26,27 +31,38 @@ public class MainActivity extends ImmersiveActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        card1 = (Button) findViewById(R.id.btn_card1);
-        card2 = (Button) findViewById(R.id.btn_card2);
-        card3 = (Button) findViewById(R.id.btn_card3);
-        card4 = (Button) findViewById(R.id.btn_card4);
-        card5 = (Button) findViewById(R.id.btn_card5);
-        card6 = (Button) findViewById(R.id.btn_card6);
-        card7 = (Button) findViewById(R.id.btn_card7);
-        card8 = (Button) findViewById(R.id.btn_card8);
-        card9 = (Button) findViewById(R.id.btn_card9);
-        card10 = (Button) findViewById(R.id.btn_card10);
 
-        card1.setOnClickListener(this);
-        card2.setOnClickListener(this);
-        card3.setOnClickListener(this);
-        card4.setOnClickListener(this);
-        card5.setOnClickListener(this);
-        card6.setOnClickListener(this);
-        card7.setOnClickListener(this);
-        card8.setOnClickListener(this);
-        card9.setOnClickListener(this);
-        card10.setOnClickListener(this);
+        card1 = (Button) findViewById(R.id.btn_card1);
+        card1.setOnTouchListener(listener("1"));
+        card2 = (Button) findViewById(R.id.btn_card2);
+        card2.setOnTouchListener(listener("2"));
+        card3 = (Button) findViewById(R.id.btn_card3);
+        card3.setOnTouchListener(listener("3"));
+        card4 = (Button) findViewById(R.id.btn_card4);
+        card4.setOnTouchListener(listener("4"));
+        card5 = (Button) findViewById(R.id.btn_card5);
+        card5.setOnTouchListener(listener("5"));
+        card6 = (Button) findViewById(R.id.btn_card6);
+        card6.setOnTouchListener(listener("6"));
+        card7 = (Button) findViewById(R.id.btn_card7);
+        card7.setOnTouchListener(listener("7"));
+        card8 = (Button) findViewById(R.id.btn_card8);
+        card8.setOnTouchListener(listener("8"));
+        card9 = (Button) findViewById(R.id.btn_card9);
+        card9.setOnTouchListener(listener("9"));
+        card10 = (Button) findViewById(R.id.btn_card10);
+        card10.setOnTouchListener(listener("10"));
+
+//        card1.setOnClickListener(this);
+//        card2.setOnClickListener(this);
+//        card3.setOnClickListener(this);
+//        card4.setOnClickListener(this);
+//        card5.setOnClickListener(this);
+//        card6.setOnClickListener(this);
+//        card7.setOnClickListener(this);
+//        card8.setOnClickListener(this);
+//        card9.setOnClickListener(this);
+//        card10.setOnClickListener(this);
 
         if (!OcrManager.isInit) {
             OcrManager.init(this);
@@ -54,59 +70,59 @@ public class MainActivity extends ImmersiveActivity implements View.OnClickListe
 
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(getApplicationContext(),DrawActivity.class);
-        if (view.getId() == R.id.btn_card1) {
-            intent.putExtra("intent_var","1");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card2) {
-            intent.putExtra("intent_var","2");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card3) {
-            intent.putExtra("intent_var","3");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card4) {
-            intent.putExtra("intent_var","4");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card5) {
-            intent.putExtra("intent_var", "5");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card6) {
-            intent.putExtra("intent_var","6");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card7) {
-            intent.putExtra("intent_var","7");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card8) {
-            intent.putExtra("intent_var","8");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card9) {
-            intent.putExtra("intent_var","9");
-            startActivity(intent);
-            //finish();
-        }
-        if (view.getId() == R.id.btn_card10) {
-            intent.putExtra("intent_var", "10");
-            startActivity(intent);
-            //finish();
-        }
-    }
+//    public void onClick(View view) {
+//        Intent intent = new Intent(getApplicationContext(),DrawActivity.class);
+//        if (view.getId() == R.id.btn_card1) {
+//            intent.putExtra("intent_var","1");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card2) {
+//            intent.putExtra("intent_var","2");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card3) {
+//            intent.putExtra("intent_var","3");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card4) {
+//            intent.putExtra("intent_var","4");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card5) {
+//            intent.putExtra("intent_var", "5");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card6) {
+//            intent.putExtra("intent_var","6");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card7) {
+//            intent.putExtra("intent_var","7");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card8) {
+//            intent.putExtra("intent_var","8");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card9) {
+//            intent.putExtra("intent_var","9");
+//            startActivity(intent);
+//            //finish();
+//        }
+//        if (view.getId() == R.id.btn_card10) {
+//            intent.putExtra("intent_var", "10");
+//            startActivity(intent);
+//            //finish();
+//        }
+//    }
 
     @Override
     protected void onPause() {
@@ -124,6 +140,54 @@ public class MainActivity extends ImmersiveActivity implements View.OnClickListe
         }
 
     }
+
+    public View.OnTouchListener listener(String number) {
+        final String num = number;
+    View.OnTouchListener listener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            int x=0;
+            int y=0;
+            switch (event.getAction()) {
+
+                case MotionEvent.ACTION_DOWN:
+                    view.setScaleX(0.8f);
+                    view.setScaleY(0.8f);
+                    x = view.getLeft();
+                    y = view.getTop();
+                    rect = new Rect(x,y,x+view.getWidth(),y+view.getHeight());
+                    return false;
+
+                case MotionEvent.ACTION_UP:
+                    view.setScaleX(1f);
+                    view.setScaleY(1f);
+                    if(rect.contains(x + (int) event.getX(), y + (int) event.getY())) {
+                        Intent intent = new Intent(getApplicationContext(),DrawActivity.class);
+                        intent.putExtra("intent_var",num);
+                        startActivity(intent);
+                        break;
+                    }
+                    return true;
+
+                case MotionEvent.ACTION_MOVE:
+                    if(!rect.contains(x + (int) event.getX(), y + (int) event.getY())){
+                        view.setScaleX(1f);
+                        view.setScaleY(1f);
+                        return true;
+                    }
+
+                case MotionEvent.ACTION_CANCEL:
+                    if(!rect.contains(x + (int) event.getX(), y + (int) event.getY())) {
+                        view.setScaleX(1f);
+                        view.setScaleY(1f);
+                        return true;
+                    }
+            }
+            return false;
+        }
+    };
+    return listener;
+}
 
     /**TODO:
      * Dont forget to clean the BaseAPI after going back from this screen!!
